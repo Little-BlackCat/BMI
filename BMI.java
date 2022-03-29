acda6d4220e9d278e6779ab74645a43d713cc166
Banyawat Yaisingson <89638028+Little-BlackCat@users.noreply.github.com>
2022-03-30 01:14:20 +0700

    Update BMI.java

--- BMI.java
+++ BMI.java
@@ -3,7 +3,7 @@ public class BMI {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.print("Input your weight : ");
+    System.out.print("Input your weight(kg) : ");
     float weight = sc.nextFloat();
 
     System.out.print("Input your height(cm) : ");
