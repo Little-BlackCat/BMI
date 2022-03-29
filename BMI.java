5e7bff39dfc33e97e5f5b06c9efacae61c85bf33
Banyawat Yaisingson <89638028+Little-BlackCat@users.noreply.github.com>
2022-03-30 01:12:45 +0700

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,19 @@
+package ProjecJava;
+import java.util.Scanner;
+public class BMI {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Input your weight : ");
+    float weight = sc.nextFloat();
+
+    System.out.print("Input your height(cm) : ");
+    float height = sc.nextFloat();
+    height /= 100;
+
+    final float BMI = weight/(height*height);
+
+    System.out.println("Your BMI is " + BMI + ".");
+
+  }
+}
